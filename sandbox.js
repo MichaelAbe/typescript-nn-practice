90b8fb90b1f2f598de13bbb9f450daf64385a7c1
MichaelAbe <michael.k.abe@gmail.com>
2021-07-01 13:28:05 -0400

    types and compiler

---
+++ sandbox.js
@@ -0,0 +1,16 @@
+var char = "Peeley";
+var age = 5;
+var killer = false;
+var other = "Jonsey";
+console.log(char, other);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
+char = 'yeet';
+age = 23;
+var circum = function (diamater) {
+    return diamater * Math.PI;
+};
+console.log(circum(age));
---
+++ sandbox.ts
@@ -0,0 +1,28 @@
+let char = "Peeley"
+
+let age = 5
+
+let killer = false
+
+const other = "Jonsey"
+
+console.log(char, other)
+
+const inputs = document.querySelectorAll('input')
+
+console.log(inputs)
+
+inputs.forEach(input => {
+    console.log(input)
+}
+    )
+
+char = 'yeet'
+
+age = 23
+
+let circum = (diamater: number) => {
+    return diamater * Math.PI
+}
+
+console.log(circum(age))
